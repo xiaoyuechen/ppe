ea461cf01ab62a870e01cd65e869a31a6a94bea2
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2021-10-01 13:42:08 +0200

    Update gpu_cores.sh

--- video_compression/c_encoder/ppe/gpu_cores.sh
+++ video_compression/c_encoder/ppe/gpu_cores.sh
@@ -2,7 +2,8 @@
 
 for num_thd in 16 32 64 128 256 512 1024
 do
-  >&2 echo "========== Work per GPU Core: $num_thd ==============="
-  ./cencoder -c --cl_num_thd=$num_thd
-  >&2 echo "================================================"
+  echo "========== Work per GPU Core: $num_thd ==============="
+  ./cencoder -c --cl_num_thd=$num_thd > outputs
+  cat ../../outputs/execution_stats.txt
+  echo "================================================"
 done
