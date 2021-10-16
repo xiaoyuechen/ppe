87a3ef5d4b2eb120a37307140c17588bec417e57
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2021-10-16 15:07:48 +0200

    Add gpubench skeleton

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((lice:default-license . "gpl-3.0")
+	 (lice:copyright-holder . "Xiaoyue Chen and Hannah Atmer"))))
---
+++ lab3/gpubench/gpubench.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+#include "benchmark.h"
+
+int main(int argc, char* argv[argc + 1])
+{
+  exit(EXIT_SUCCESS);
+}
